5a72024d40977e9569cd52ae877d2f66119abf35
hemanthnaika <hemanthnaika22@gmail.com>
2022-02-22 10:31:32 +0530

    change

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.use("/api/v1/answers", answersRoutes)
 app.get('/', (req, res) => {
     res.send(`Server running at ${port}`)
 })
-
+console.log('hi')
 app.listen(port, (req, res) => {
     console.log(`Server listening at PORT ${port}`)
 })
